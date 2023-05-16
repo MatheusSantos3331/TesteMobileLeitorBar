acadf6e66fe0defa40f62908dd164e9f7691dc39
MATHEUS FRANCISCO FERREIRA DOS SANTOS <116918467+MatheusSantos3331@users.noreply.github.com>
2023-05-16 11:07:46 -0300

    Add files via upload

--- barcode.js
+++ barcode.js
@@ -47,10 +47,10 @@ var barcode = function() {
 
 	var config = {
 		strokeColor: '#f00',
-		start: 0.2,
-		end: 0.6,
+		start: 0,
+		end: 1,
 		threshold: 160,
-		quality: 0.4,
+		quality: 0.5,
 		delay: 100,
 		video: '',
 		canvas: '',
